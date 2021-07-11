d62654536fbe1025b50fce3f95676b5a8754960c
andy <agi2000@mail.ru>
2021-07-11 17:27:48 +0300

    video

--- residence.js
+++ residence.js
@@ -1,4 +1,4 @@
-let numPic = 10;
+let numPic = 5;
 const matrix = [
     [0, 0, 75, 75],
     [0, 0, 50, 50],
@@ -28,8 +28,9 @@ const matrix = [
     [75, 75, 0, 0]
 ];
 let currentPic = 0;
+let currentVid = 0;
 const imageArray = [];
-
+const videoArray = [];
 
 document.addEventListener("DOMContentLoaded", function () {
     const imageList = document.querySelectorAll('.image_item');
@@ -38,34 +39,80 @@ document.addEventListener("DOMContentLoaded", function () {
         imageArray.push(item.dataset.link);
     });
     shuffle(imageArray);
-    numPic = numPic > imageArray.length ? imageArray.length : numPic;
+
+    const videoList = document.querySelectorAll('.video_item');
+    
+    Array.from(videoList, item => {
+        videoArray.push(item.dataset.link);
+    });
+    shuffle(videoArray);
+
+    // numPic = numPic > imageArray.length ? imageArray.length : numPic;
     const picWrapper = document.querySelector('.pic_wrapper');
     if (picWrapper) {
         for (let ind = 0; ind < numPic; ind++) {
             createElement(ind);
-            // let div = document.createElement('div');
-            // div.className = "image_element";
-            // div.style.backgroundImage ="url(" + imageArray[ind] + ")";
-            // div.style.position ="absolute";
-            // const position = matrix[getRandomInt(0,matrix.length)];
-            // div.style.left =position[0] + "%";
-            // div.style.top =position[0]+ "%";
-            // div.style.right =position[0] + "%";
-            // div.style.bottom =position[0]+ "%" ;
-            // currentPic =ind;
-            // picWrapper.append(div);
         }
-    setInterval(changePicture, 2000);    
+    //setInterval(changePicture, 2000);    
     }
     
-    const media = document.querySelector('video');
-    const mediaWrapper = document.querySelector('.video_wrapper');
-    setPosition(mediaWrapper);
+     const media = document.querySelector('video');
+    // const mediaWrapper = document.querySelector('.video_wrapper');
+    // setPosition(mediaWrapper);
     media.play();
     media.addEventListener('ended', () => {
-        setPosition(mediaWrapper);
-        media.play();
+        //setPosition(mediaWrapper);
+        document.querySelector('.video_wrapper').style.display = 'none';
+        setTimeout(() => {
+            currentVid++;
+            if (currentVid > videoArray.length -1) {
+                currentVid = 0;
+            }
+            const sourceVid = document.querySelector('.video_wrapper video');
+            sourceVid.setAttribute("src", videoArray[currentVid]);
+            document.querySelector('.video_wrapper').style.display = 'block';
+            media.load();
+            media.play();
+        }, 1500);
+        //media.play();
     });
+    // $('#listpic').endless({
+
+    //     direction:'vertical', //Direction : up (infinite top scrolling), down (infinite bottom scrolling), vertical (infinite top and bottom scrolling)
+        
+    //     scrollbar:'enable', //Enable or disable the scrollbar
+        
+    //     prepend:function(){alert("there isn't the boundary at the top");}, //Running when content is prepended
+        
+    //     append:function(){alert("there isn't the boundary at the bottom");}, //Running when content is appended
+        
+    //     n_prepend:function(){alert("you are now at the top");}, //Running when content in duplicate is removed and that you come from the top
+        
+    //     n_append:function(){alert("you are now at the bottom");} //Running when content in duplicate is removed and that you come from the bottom
+        
+    //     });
+
+    // $('.image_list').infiniteScroll({
+    //     // options
+    //     path: '.pagination__next',
+    //     append: '.image_item',
+    //     history: false,
+    //   });
+    //$('#listpic').jscroll();
+
+    const div = document.getElementById( "pictures" );
+   
+    div.addEventListener( "scroll", function() {
+        //var wrapper = document.querySelector('.pic_wrapper');
+      const max_scroll = this.scrollHeight - this.clientHeight;
+      const current_scroll = this.scrollTop;
+      const bottom = 100;
+      if ( current_scroll + bottom >= max_scroll ) {
+      
+        changePicture();
+      }
+    } );
+
 });
 
 function getRandomInt(min, max) {
@@ -98,21 +145,18 @@ function createElement( ind) {
     let div = document.createElement('div');
     div.className = "image_element";
     div.style.backgroundImage = "url(" + imageArray[ind] + ")";
-    div.style.position = "absolute";
+    div.style.position = "relative";
     setPosition(div);
-    // const position = matrix[getRandomInt(0, matrix.length)];
-    // div.style.left = position[0] + "%";
-    // div.style.top = position[1] + "%";
-    // div.style.right = position[2] + "%";
-    // div.style.bottom = position[3] + "%";
+    
     currentPic = ind+1;
     picWrapper.append(div);
 
 }
 function setPosition(element) {
     const position = matrix[getRandomInt(0, matrix.length)];
-    element.style.left = position[0] + "%";
-    element.style.top = position[1] + "%";
-    element.style.right = position[2] + "%";
-    element.style.bottom = position[3] + "%"; 
+    const newWidth = getRandomInt(30, 80);
+    element.style.width = newWidth + "%";
+    element.style.height = (newWidth * 9 /16) + "%";
+    element.style.left = getRandomInt(0, 100 -newWidth) + "%";
+    
 }
