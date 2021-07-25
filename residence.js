97dfd9cc745c0b7fa3457044a57a813c8d816ed9
andy <agi2000@mail.ru>
2021-07-25 11:05:53 +0300

    tst

--- residence.js
+++ residence.js
@@ -1,32 +1,4 @@
 let numPic = 8;
-// const matrix = [
-//     [0, 0, 75, 75],
-//     [0, 0, 50, 50],
-//     [0, 0, 25, 25],
-//     [25, 0, 50, 75],
-//     [25, 0, 25, 50],
-//     [25, 0, 0, 25],
-//     [50, 0, 25, 75],
-//     [50, 0, 0, 50],
-//     [75, 0, 0, 75],
-//     [0, 25, 75, 50],
-//     [0, 25, 50, 25],
-//     [0, 25, 25, 0],
-//     [25, 25, 50, 50],
-//     [25, 25, 25, 25],
-//     [25, 25, 0, 0],
-//     [50, 25, 25, 50],
-//     [50, 25, 0, 25],
-//     [0, 50, 75, 25],
-//     [0, 50, 50, 0],
-//     [25, 50, 50, 25],
-//     [25, 50, 25, 0],
-//     [75, 50, 0, 25],
-//     [0, 75, 75, 0],
-//     [25, 75, 50, 0],
-//     [50, 75, 25, 0],
-//     [75, 75, 0, 0]
-// ];
 let currentPic = 0;
 let currentVid = 0;
 const imageArray = [];
@@ -34,157 +6,137 @@ const videoArray = [];
 const paralaxArray = [];
 var lastScrollTop = 0;
 const SAFETY_MARGIN = 150;
+var detector;
 
 document.addEventListener("DOMContentLoaded", function () {
-        const imageList = document.querySelectorAll('.image_item, .video_item');
+    const imageList = document.querySelectorAll('.image_item, .video_item');
+     const options = {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0.1
+    }
 
-        Array.from(imageList, item => {
-                imageArray.push({
-                        class: item.className,
-                        link: item.dataset.link});
-                }); shuffle(imageArray);
+    // const targetNode = document.getElementById('pictures');
 
-            // const videoList = document.querySelectorAll('.video_item');
+    // // Options for the observer (which mutations to observe)
+    // const config = { attributes: true, childList: true, subtree: true };
+    
+    // // Callback function to execute when mutations are observed
+    // const callback = function(mutationsList, observer) {
+    //     // Use traditional 'for loops' for IE 11
+    //     for(const mutation of mutationsList) {
+    //         if (mutation.type === 'childList') {
+    //             console.log('A child node has been added or removed.');
+    //         }
+    //         else if (mutation.type === 'attributes') {
+    //             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+    //         }
+    //     }
 
-            // Array.from(videoList, item => {
-            //     videoArray.push(item.dataset.link);
-            // });
-            // shuffle(videoArray);
+    // };
+    
+    // // Create an observer instance linked to the callback function
+    // const observer = new MutationObserver(callback);
+    
+    // // Start observing the target node for configured mutations
+    // observer.observe(targetNode, config);
 
-            // numPic = numPic > imageArray.length ? imageArray.length : numPic;
-            const picWrapper = document.querySelector('.pic_frame');
-            if (picWrapper) {
-                for (let ind = 0; ind < numPic; ind++) {
-                    // const newDiv = createElement(ind, false);
-                    // picWrapper.append(newDiv);
-                    appendPicture();
 
+    detector = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            let newDiv;
+            if (entry.isIntersecting) {
+                let isUpper = entry.boundingClientRect.y < entry.rootBounds.y;
+                let curScroll;
+                if ($(entry.target).find('img,video').first().is(paralaxArray[0].elements[0])) {
+                    newDiv = prependPicture(true);
+                } else if ($(entry.target).find('img,video').first().is(paralaxArray[paralaxArray.length - 1].elements[0])) {
+                    //disableScrolling();
+                    var block = document.querySelector('.pic_frame');
+                    var lastScrollHeight = block.scrollHeight;
+                    
+                    newDiv = appendPicture(true);
+                    //enableScrolling();
+                    block.scrollTop += (block.scrollHeight-lastScrollHeight);
                 }
-                //setInterval(changePicture, 2000);    
+                // }
+                console.log(entry);
+            }
+            //console.log(entry);
+            if (newDiv) {
+                detector.observe(newDiv);
             }
 
-            // const media = document.querySelector('video');
-            // media.play();
-            // media.addEventListener('ended', () => {
-            //     document.querySelector('.video_wrapper').style.display = 'none';
-            //     setTimeout(() => {
-            //         currentVid++;
-            //         if (currentVid > videoArray.length - 1) {
-            //             currentVid = 0;
-            //         }
-            //         const sourceVid = document.querySelector('.video_wrapper video');
-            //         sourceVid.setAttribute("src", videoArray[currentVid]);
-            //         document.querySelector('.video_wrapper').style.display = 'block';
-            //         media.load();
-            //         media.play();
-            //     }, 1500);
-            // });
+        })
+    }, options);
 
-            // window.addEventListener('scroll', function () {
-            //     console.log('wi scroll');
-            // })
 
-            const div = document.getElementById("pictures");
-
-            div.addEventListener("scroll", function () {
-                var wrapper = document.querySelector('.pic_frame');
-                //const st = window.pageYOffset || document.documentElement.scrollTop;
-                const max_scroll = this.scrollHeight - this.clientHeight;
-                const st = this.scrollTop;
-                const bottom = 100;
-                if (st > lastScrollTop) {
-                    if (st + bottom >= max_scroll) {
-
-                        appendPicture(true);
-                    }
-                } else if (st < lastScrollTop) {
-                    if (st - bottom <= 0) {
-                        prependPicture(true);
-                    }
-                }
-                lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-
-                let scrollBottom = this.scrollHeight - this.scrollTop - this.clientHeight;
-                if (scrollBottom <= 0) {
-                    this.scrollBy(0,-50);
-                }
-                if (st <= 0) {
-                    this.scrollBy(0,50);
-                } 
-                
-                //     scrollPos = $(this).scrollTop(),
-                //     docHeight = $(document.body).height(),
-                //     winHeight = $(window).height(),
-                //     lowerLimit = SAFETY_MARGIN,
-                //     higherLimit = docHeight - SAFETY_MARGIN;
-
-                //      // Scrolling too high
-                // if( scrollPos <= lowerLimit ){
-
-                //     // Move content to top;
-                //     //$(lastDiv).prependTo(document.body);
-                //     prependPicture();
-                //     // Adjust scroll position to compensate
-                //     // for the new content at the top
-                //     //$(window).scrollTop(scrollPos + $(lastDiv).height());
-
-                // }
-
-                // // Scrolling too low
-                // else if( scrollPos + winHeight >= higherLimit ){
-
-                //     // Move content to bottom
-                //     //$(firstDiv).appendTo(document.body);
-                //     appendPicture();
-                //     // Adjust scroll position to compensate
-                //     // for the missing content at the top
-                //    // $(window).scrollTop(scrollPos - $(firstDiv).height());
-                // } 
-
-            });
-
+    Array.from(imageList, item => {
+        imageArray.push({
+            class: item.className,
+            link: item.dataset.link
         });
+    });
+    shuffle(imageArray);
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+    // const videoList = document.querySelectorAll('.video_item');
 
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
+    // Array.from(videoList, item => {
+    //     videoArray.push(item.dataset.link);
+    // });
+    // shuffle(videoArray);
 
-    function appendPicture(shift = false) {
-        const picElement = document.querySelector('.image_element');
-        const picWrapper = document.querySelector('.pic_frame');
-        if (picElement && shift) {
-            picElement.remove();
+    // numPic = numPic > imageArray.length ? imageArray.length : numPic;
+    const picWrapper = document.querySelector('.pic_frame');
+    if (picWrapper) {
+        for (let ind = 0; ind < numPic; ind++) {
+            appendPicture();
+
         }
-        if (currentPic + 1 > imageArray.length - 1) {
-            currentPic = 0
-        }
-        const newDiv = createElement(currentPic, true);
-        picWrapper.append(newDiv);
-        const imdToParallax = newDiv.firstChild; //querySelector('img');
-        if (imdToParallax) {
-        const container = document.querySelector('.pic_frame');
-        const newParallax = new simpleParallax(imdToParallax, {
-            customContainer: container,
-            customWrapper: '.image_element',
-            scale: 2.3,
-            orientation: 'down'
-        });
-        paralaxArray.push(newParallax);
-        if (shift) {
-            paralaxArray[0].destroy();
-            paralaxArray.shift();
-        }
+
+
     }
+    
+});
+
+window.onload = () => {
+    const addedList = document.querySelectorAll('.image_element');
+
+    Array.from(addedList, item => {
+        detector.observe(item);
+    });
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function appendPicture(shift = false) {
+    const picElement = document.querySelector('.image_element');
+    const picWrapper = document.querySelector('.pic_frame');
+    if (picElement && shift) {
+        detector.unobserve(picElement);
+        picElement.remove();
+    }
+    if (currentPic + 1 > imageArray.length - 1) {
+        currentPic = 0
+    }
+    const newDiv = createElement(currentPic, true);
+    picWrapper.append(newDiv);
+    const imdToParallax = newDiv.firstChild; //querySelector('img');
+    makeParallax(true, imdToParallax, shift);
     currentPic += 1;
+
+    return newDiv;
 
 }
 
@@ -192,6 +144,7 @@ function prependPicture(shift = false) {
     const picElement = document.querySelector('.image_element:last-child');
     const picWrapper = document.querySelector('.pic_frame');
     if (picElement && shift) {
+        detector.unobserve(picElement);
         picElement.remove();
     }
     if (currentPic - 1 < 0) {
@@ -199,23 +152,10 @@ function prependPicture(shift = false) {
     }
     const newDiv = createElement(currentPic, true);
     picWrapper.prepend(newDiv);
-    const imdToParallax = newDiv.firstChild;   //querySelector('img');
-    if (imdToParallax) {
-        const container = document.querySelector('.pic_frame');
-        const newParallax = new simpleParallax(imdToParallax, {
-            customContainer: container,
-            customWrapper: '.image_element',
-            scale: 2.3,
-            orientation: 'down'
-        });
-        paralaxArray.push(newParallax);
-        if (shift) {
-            paralaxArray[0].destroy();
-            paralaxArray.shift();
-        }
-    }
+    const imdToParallax = newDiv.firstChild; //querySelector('img');
+    makeParallax(false, imdToParallax, shift);
     currentPic -= 1;
-
+    return newDiv;
 }
 
 function createElement(ind) {
@@ -225,16 +165,15 @@ function createElement(ind) {
     let newImg;
     if (imageArray[ind].class.includes('image_item')) {
         newImg = document.createElement('img');
-       
+
     } else {
         newImg = document.createElement('video');
         newImg.muted = true;
         newImg.autoplay = true;
         newImg.setAttribute('type', "video/mp4")
-        
+
     }
     newImg.src = imageArray[ind].link;
-    // div.style.backgroundImage = "url(" + imageArray[ind] + ")";
     div.style.position = "relative";
     setPosition(div);
 
@@ -250,3 +189,47 @@ function setPosition(element) {
     element.style.left = getRandomInt(0, 100 - newWidth) + "%";
 
 }
+
+function makeParallax(append, element, shift) {
+    if (element) {
+        const container = document.querySelector('.pic_frame');
+        const newParallax = new simpleParallax(element, {
+            customContainer: container,
+            customWrapper: '.image_element',
+            scale: 2.3,
+            orientation: 'down'
+        });
+        if (append) {
+            paralaxArray.push(newParallax);
+            if (shift) {
+                paralaxArray[0].destroy();
+                paralaxArray.shift();
+            }
+        } else {
+            paralaxArray.unshift(newParallax);
+            if (shift) {
+                paralaxArray[paralaxArray.length-1].destroy();
+                paralaxArray.pop();
+            }
+        }
+
+        paralaxArray.push(newParallax);
+        if (shift) {
+            paralaxArray[0].destroy();
+            paralaxArray.shift();
+        }
+    }
+}
+
+function disableScrolling(){
+    var block = document.querySelector('.pic_frame');
+    var x=block.scrollX;
+    var y=block.scrollY;
+    block.onscroll=function(){block.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+    var block = document.querySelector('.pic_frame');
+    block.onscroll=function(){};
+}
+
---
+++ tst.js
@@ -0,0 +1,205 @@
+var numPic = 8;
+var currentPic = 0;
+var imageArray = [];
+var sceneArray = [];
+
+jQuery(document).ready(function ($) {
+
+    var controller = new ScrollMagic.Controller();
+
+    $('.image_item, .video_item').each(function () {
+        imageArray.push(this);
+    });
+    shuffle(imageArray);
+
+    // build scene
+    var scene_up = new ScrollMagic.Scene({
+            triggerElement: "#loader_up",
+            triggerHook: 0,
+            offset: "50px",
+            reverse: true
+        })
+        .addTo(controller)
+        .on("leave", function (e) {
+            createElement('up', true);
+            scene_up.update();
+            
+        })
+        .addIndicators();
+
+    var scene_down = new ScrollMagic.Scene({
+            triggerElement: "#loader_down",
+            triggerHook: "onEnter"
+        })
+        .addTo(controller)
+        .on("enter", function (e) {
+            createElement('down', true);
+            scene_down.update();
+            
+        })
+        .addIndicators();
+
+    // add some boxes to start with.
+    addBoxes(8);
+    $('#pictures').on('scroll', function() {
+        if ($('#pictures').scrollTop() == 0) {
+            $('#pictures').scrollTop(50);
+        }
+    });
+
+    //$(document).on('load', 'img', newPosition);
+    //$('.image_element').on('load', 'img', newPosition );
+    // document.addEventListener('load',function(e){
+    //     if(e.target && e.target.tagName == 'IMG'){
+    //           newPosition();
+    //      }
+    //  });
+        
+    // pseudo function to add new content. In real life it would be done through an ajax request.
+    function addBoxes(amount) {
+        for (i = 1; i <= amount; i++) {
+            createElement('down', false);
+        }
+        // "loading" done -> revert to normal state
+        scene_down.update(); // make sure the scene gets the new start position
+    }
+
+
+    function createElement(direction, remove) {
+        // var loaderElement = $('#loader_down');
+        var newElement;
+        if ($(imageArray[currentPic]).hasClass('image_item')) {
+            newElement = $('.templates .picture_element').clone();
+            if (direction === 'up') {
+                newElement.insertAfter($('#loader_up'));
+            } else if (direction === 'down') {
+                newElement.insertBefore($('#loader_down'));
+                // $('#pictures .image_element').first().remove();
+            }
+            newElement.children('img').attr('src', imageArray[currentPic].dataset.link);
+            newElement.children('img').attr('data-width', imageArray[currentPic].dataset.width)
+            newElement.children('img').attr('data-height', imageArray[currentPic].dataset.height)
+        } else {
+            newElement = $('.templates .video_element').clone();
+            if (direction === 'up') {
+                newElement.insertAfter($('#loader_up'));
+            } else if (direction === 'down') {
+                newElement.insertBefore($('#loader_down'));
+            }
+            newElement.children('video').attr('src', imageArray[currentPic].dataset.link);
+        }
+        setPosition(newElement);
+        if (remove) {
+            if (direction === 'up') {
+                $('#pictures .image_element').last().remove();
+               // console.log('---', newElement.height());
+                $('#pictures').scrollTop(newElement.outerHeight());
+            } else if (direction === 'down') {
+                $('#pictures .image_element').first().remove();
+            }
+        }
+
+        // var picWrapper = document.querySelector('.pic_frame');
+        // var newElement = document.createElement('div');
+        // newElement.className = "image_element";
+        // var newImg;
+        // if (imageArray[currentPic].className.includes('image_item')) {
+        //     newImg = document.createElement('img');
+    
+        // } else {
+        //     newImg = document.createElement('video');
+        //     newImg.muted = true;
+        //     newImg.autoplay = true;
+        //     newImg.setAttribute('type', "video/mp4")
+    
+        // }
+        // newElement.append(newImg);
+        // newElement.style.position = "relative";
+        // newImg.src = imageArray[currentPic].dataset.link;
+        // var loader;
+        // if (direction === 'up') {
+        //     loader = document.querySelector('#loader_up');
+        //     loader.after(newElement);
+        //     //         newElement.insertAfter($('#loader_up'));
+        // } else if (direction === 'down') {
+        //     loader = document.querySelector('#loader_down');
+        //     loader.before(newElement);
+        //     //         newElement.insertBefore($('#loader_down'));
+        //     //         // $('#pictures .image_element').first().remove();
+        // }
+
+
+        
+       
+        //setPosition(div);
+    
+        //div.append(newImg);
+        //return div;
+
+
+        if (currentPic + 1 > imageArray.length - 1) {
+            currentPic = 0
+        } else {
+            currentPic++;
+        }
+        return newElement;
+
+    }
+
+    function setPosition(element) {
+        var newWidth = getRandomInt(30, 80);
+        var aspectRatio;
+        element.css('position', 'relative');
+        if (element.find('video').length) {
+            element.css('width', newWidth + "%");
+            element.css('height', (newWidth * 9 / 16) + "%");
+        } else {
+            if (element.find('img').attr('data-width') > 0) {
+                aspectRatio = element.find('img').attr('data-height') / element.find('img').attr('data-width');
+                if (aspectRatio > 1) {
+                    element.css('width', newWidth + "%");
+                    //element.css('height', (newWidth / aspectRatio) + "%");
+                    element.css('height', "auto");
+                } else {
+                    element.css('height', newWidth + "%");
+                    //element.css('width', (newWidth * aspectRatio) + "%");
+                    element.css('width', "auto");
+                }
+
+            }
+        }
+        element.css('left', getRandomInt(0, 100 - newWidth) + "%");
+
+        // const newWidth = getRandomInt(30, 80);
+        // element.css('position', 'relative');
+        // element.css('width', newWidth + "%");
+        // element.css('height', (newWidth * 9 / 16) + "%");
+        // element.css('left', getRandomInt(0, 100 - newWidth) + "%");
+
+    }
+
+    function newPosition() {
+        
+        console.log('this');
+        // const newWidth = getRandomInt(30, 80);
+        // element.css('position', 'relative');
+        // element.css('width', newWidth + "%");
+        // element.css('height', (newWidth * 9 / 16) + "%");
+        // element.css('left', getRandomInt(0, 100 - newWidth) + "%");
+
+    }
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+});
